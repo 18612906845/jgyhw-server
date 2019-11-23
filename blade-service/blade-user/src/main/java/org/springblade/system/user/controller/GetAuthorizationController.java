3b82907d4b087a198408628b3a32c72b44391c27
wanglei <296831181@qq.com>
2019-11-23 18:00:12 +0800

    完成公众号登录操作后页面的跳转

--- blade-service/blade-user/src/main/java/org/springblade/system/user/controller/GetAuthorizationController.java
+++ blade-service/blade-user/src/main/java/org/springblade/system/user/controller/GetAuthorizationController.java
@@ -5,17 +5,18 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springblade.core.tool.api.R;
 import org.springblade.system.user.entity.WxUser;
 import org.springblade.system.user.service.IWxUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +28,7 @@ import java.util.Map;
  */
 @Slf4j
 @RefreshScope
-@RestController
+@Controller
 @RequestMapping("/getAuthorization")
 @Api(value = "获取授权，取得用户信息", tags = "获取授权，取得用户信息")
 public class GetAuthorizationController {
@@ -44,37 +45,49 @@ public class GetAuthorizationController {
 	@Value("${jgyhw.wxGzh.appSecret:}")
 	private String wxGzhAppSecret;
 
+	@Value("${jgyhw.wxGzh.loginResultPageUrl:}")
+	private String loginResultPageUrl;
+
 	@Autowired
 	private RestTemplate restTemplate;
 
 	@Autowired
 	private IWxUserService wxUserService;
 
-    /**
-     * 通过临时凭证Code获取微信公众号用户信息
-     *
-     * @param request
-     * @return
-     */
-    @RequestMapping("/getWxGzhUserInfoByCode")
-    public R getWxGzhUserInfoByCode(HttpServletRequest request){
-        String code = request.getParameter("code");
-        Map<String, String> accessTokenOpenIdMap = getAccessTokenAndOpenIdByCode(code);
-        if(accessTokenOpenIdMap != null) {
+	/**
+	 * 通过临时凭证Code获取微信公众号用户信息
+	 *
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("/getWxGzhUserInfoByCode")
+	public String getWxGzhUserInfoByCode(HttpServletRequest request){
+		String resultPageUrl = loginResultPageUrl;
+
+		String code = request.getParameter("code");
+		Map<String, String> accessTokenOpenIdMap = getAccessTokenAndOpenIdByCode(code);
+		if(accessTokenOpenIdMap != null) {
 			String url = wxGzhGetUserInfoReqUrl;
 			if (StringUtils.isBlank(url)) {
-				return R.status(false);
+				resultPageUrl = resultPageUrl.replaceAll("NIKE_NAME", "");
+				resultPageUrl = resultPageUrl.replaceAll("HEAD_IMG", "");
+				resultPageUrl = resultPageUrl.replaceAll("STATUS", "false");
+				return resultPageUrl;
 			}
 			url = url.replaceAll("ACCESS_TOKEN", accessTokenOpenIdMap.get("accessToken"));
 			url = url.replaceAll("OPENID", accessTokenOpenIdMap.get("openId"));
 
-			JSONObject respJsonObj = restTemplate.getForObject(url, JSONObject.class);
-			log.info("通过临时凭证Code获取微信公众号用户信息结果：" + respJsonObj);
+			String resp = restTemplate.getForObject(url, String.class);
+			log.info("通过临时凭证Code获取微信公众号用户信息结果：" + resp);
 
-			if (respJsonObj != null) {
-				if (respJsonObj.containsKey("errcode")) {
+			if (StringUtils.isNotBlank(resp)) {
+				JSONObject respJsonObj = JSONObject.parseObject(resp);
+				if (StringUtils.isNotBlank(respJsonObj.getString("errcode"))) {
 					log.error("通过临时凭证Code获取微信公众号用户信息，错误编号：" + respJsonObj.getString("errcode") + "；错误描述：" + respJsonObj.getString("errmsg"));
-					return R.status(false);
+					resultPageUrl = resultPageUrl.replaceAll("NIKE_NAME", "");
+					resultPageUrl = resultPageUrl.replaceAll("HEAD_IMG", "");
+					resultPageUrl = resultPageUrl.replaceAll("STATUS", "false");
+					return resultPageUrl;
 				} else {
 					// 保存用户信息
 					String unionId = respJsonObj.getString("unionid");
@@ -93,42 +106,55 @@ public class GetAuthorizationController {
 					wu.setUpdateTime(new Date());
 					wxUserService.saveOrUpdate(wu);
 					log.info("用户登陆成功，创建/更新用户信息：" + wu.toString());
-					// TODO 跳转登陆成功页面
-
-					// resultPageUrl = "redirect:/wxGzhMessageSubscribe/openMessageSubscribeSuccessPage";
-					return R.status(true);
+					String nickName = "";
+					try {
+						nickName = URLEncoder.encode(wu.getNickName(), "utf-8");
+					} catch (UnsupportedEncodingException e) {
+						log.error("登陆结果跳转链接用户昵称Encode编码异常", e);
+					}
+					resultPageUrl = resultPageUrl.replaceAll("NIKE_NAME", nickName);
+					resultPageUrl = resultPageUrl.replaceAll("HEAD_IMG", wu.getHeadImgUrl());
+					resultPageUrl = resultPageUrl.replaceAll("STATUS", "true");
+					return resultPageUrl;
 				}
 			} else {
 				log.error("通过临时凭证Code获取微信公众号用户信息结果为空");
-				return R.status(false);
+				resultPageUrl = resultPageUrl.replaceAll("NIKE_NAME", "");
+				resultPageUrl = resultPageUrl.replaceAll("HEAD_IMG", "");
+				resultPageUrl = resultPageUrl.replaceAll("STATUS", "false");
+				return resultPageUrl;
 			}
 		}else{
-        	return R.status(false);
+			resultPageUrl = resultPageUrl.replaceAll("NIKE_NAME", "");
+			resultPageUrl = resultPageUrl.replaceAll("HEAD_IMG", "");
+			resultPageUrl = resultPageUrl.replaceAll("STATUS", "false");
+			return resultPageUrl;
 		}
-    }
+	}
 
-    /**
-     * 根据临时凭证Code获取微信公众号access_token和openid
-     *
-     * @param code 临时凭证
-     * @return
-     */
-    private Map<String, String> getAccessTokenAndOpenIdByCode(String code){
-        Map<String, String> resultMap = new HashMap<>();
-        if(StringUtils.isBlank(code)){
-            return null;
-        }
-        String url = wxGzhGetAccessTokenOpenIdReqUrl;
-        if(StringUtils.isBlank(url)){
-            return null;
-        }
-        url = url.replaceAll("APPID", wxGzhAppId);
-        url = url.replaceAll("SECRET", wxGzhAppSecret);
-        url = url.replaceAll("CODE", code);
+	/**
+	 * 根据临时凭证Code获取微信公众号access_token和openid
+	 *
+	 * @param code 临时凭证
+	 * @return
+	 */
+	private Map<String, String> getAccessTokenAndOpenIdByCode(String code){
+		Map<String, String> resultMap = new HashMap<>();
+		if(StringUtils.isBlank(code)){
+			return null;
+		}
+		String url = wxGzhGetAccessTokenOpenIdReqUrl;
+		if(StringUtils.isBlank(url)){
+			return null;
+		}
+		url = url.replaceAll("APPID", wxGzhAppId);
+		url = url.replaceAll("SECRET", wxGzhAppSecret);
+		url = url.replaceAll("CODE", code);
 
-		JSONObject respJsonObj = restTemplate.getForObject(url, JSONObject.class);
-		log.info("根据临时凭证Code获取微信公众号access_token和openid结果：" + respJsonObj);
-		if(respJsonObj != null){
+		String resp = restTemplate.getForObject(url, String.class);
+		log.info("根据临时凭证Code获取微信公众号access_token和openid结果：" + resp);
+		if(StringUtils.isNotBlank(resp)){
+			JSONObject respJsonObj = JSONObject.parseObject(resp);
 			if(respJsonObj.containsKey("access_token") && respJsonObj.containsKey("openid")){
 				resultMap.put("accessToken", respJsonObj.getString("access_token"));
 				resultMap.put("openId", respJsonObj.getString("openid"));
@@ -141,6 +167,6 @@ public class GetAuthorizationController {
 			resultMap = null;
 		}
 		return resultMap;
-    }
+	}
 
 }
