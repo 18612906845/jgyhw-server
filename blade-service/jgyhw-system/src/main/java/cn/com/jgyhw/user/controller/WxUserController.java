a66e102d25a6ab8018cb58a10194bf75316d7905
wanglei <296831181@qq.com>
2019-12-01 02:21:37 +0800

    增加我的邀请查询

--- blade-service/jgyhw-system/src/main/java/cn/com/jgyhw/user/controller/WxUserController.java
+++ blade-service/jgyhw-system/src/main/java/cn/com/jgyhw/user/controller/WxUserController.java
@@ -2,16 +2,23 @@ package cn.com.jgyhw.user.controller;
 
 import cn.com.jgyhw.user.entity.WxUser;
 import cn.com.jgyhw.user.service.IWxUserService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springblade.core.mp.support.Condition;
+import org.springblade.core.mp.support.Query;
 import org.springblade.core.tool.api.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 微信用户控制器
@@ -96,6 +103,36 @@ public class WxUserController {
 		}else{
 			return R.data("您已经绑定过其他推荐人");
 		}
+	}
+
+	/**
+	 * 根据登陆标识查询我的邀请列表
+	 *
+	 * @param loginKey 登陆标识
+	 * @param query 分页参数对象
+	 * @return
+	 */
+	@GetMapping("/findMyInviteWxUserListByLoginKeyPage")
+	public R<Map<String, Object>> findMyInviteWxUserListByLoginKeyPage(Long loginKey, Query query){
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("isMore", false);
+		resultMap.put("msg", "未知错误");
+
+		IPage<WxUser> page = wxUserService.page(Condition.getPage(query), Wrappers.<WxUser>lambdaQuery().eq(WxUser::getParentWxUserId, loginKey));
+
+		if(page == null && CollectionUtils.isNotEmpty(page.getRecords())){
+			resultMap.put("wxUserList", new ArrayList<>());
+			return R.data(resultMap);
+		}
+
+		long count = query.getCurrent() * query.getSize();
+		if(count >= page.getTotal()){
+			resultMap.put("isMore", false);
+		}else{
+			resultMap.put("isMore", true);
+		}
+		resultMap.put("wxUserList", page.getRecords());
+		return R.data(resultMap);
 	}
 
 }
